5f9b0ec853465953a8fb1c9be10b16d69ea9b783
薛日生 <tarena035@rs.local>
2015-12-01 10:06:10 +0800

    提交

---
+++ Demo/Demo01/Demo01/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Demo01
+//
+//  Created by tarena035 on 15/12/1.
+//  Copyright © 2015年 tarena035. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
